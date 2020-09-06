18753d9791607225057ce3f7624af6e13f695f4a
Soham Das <majumder70.ranu@gmail.com>
2020-09-06 13:49:42 +0530

    Adding Portfolio

---
+++ Portfolio/precache-manifest.7ba0d89fddf30fb08fbcef6f0c3c0059.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89757bc91b2c192345530009293cd8f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ab67c178027e4d186e6",
+    "url": "/static/css/2.7be1ffac.chunk.css"
+  },
+  {
+    "revision": "9cbc95ca1564aa8c15cc",
+    "url": "/static/css/main.e4900256.chunk.css"
+  },
+  {
+    "revision": "1ab67c178027e4d186e6",
+    "url": "/static/js/2.0b56b70a.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.0b56b70a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cbc95ca1564aa8c15cc",
+    "url": "/static/js/main.0a5c62c8.chunk.js"
+  },
+  {
+    "revision": "6f66a38a23320cf835ab",
+    "url": "/static/js/runtime-main.1b2b1293.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
